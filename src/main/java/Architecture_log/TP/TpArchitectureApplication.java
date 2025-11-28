ab6afd9c216427b316312bd2273489462f2852f8
Wiam ABDELLAOUI <wiamabdellaoui66@gmail.com>
2025-11-28 08:19:30 +0100

    Added files

---
+++ src/main/java/Architecture_log/TP/TpArchitectureApplication.java
@@ -0,0 +1,13 @@
+package Architecture_log.TP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Architecture_log/TP/TpArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package Architecture_log.TP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
